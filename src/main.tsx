099e22a176e84096a3c0c35b06006aea3b31b989
Luis Enrique Gomes Portugal <luisenriquegomesportugal@gmail.com>
2023-01-29 05:54:33 -0300

    base

--- src/main.tsx
+++ src/main.tsx
@@ -24,8 +24,8 @@ addLocale('pt-BR', {
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/retiro'>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
